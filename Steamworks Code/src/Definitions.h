a5d49f082a27e17fa7f8097a75e37b6d8a04005d
Benjamin Ventimiglia <Benjaminaventi@gmail.com>
2017-02-10 13:34:03 -0500

    Renamed DriveTrain to RoboBase since it was becoming a catch all, also added a new Definitions file and removed some crap that keeps coming back

---
+++ Steamworks Code/src/Definitions.h
@@ -0,0 +1,14 @@
+/*
+ * Definitions.h
+ *
+ *  Created on: February 10, 2017
+ *      Author: Benjamin Ventimiglia
+ */
+
+#ifndef SRC_DEFINITIONS_H_
+#define SRC_DEFINITIONS_H_
+
+#define SHIFT_UP_SPEED 2900 				//Encoder speed(rate) limits for shifting gears
+#define SHIFT_DOWN_SPEED 25250
+
+#endif /* SRC_DEFINITIONS_H_ */
